0d860edc36de13fa74da7064f672e8ab2541a40f
Ruben Rmz <git@rramirez.com>
2026-01-18 02:37:23 -0600

    Updating setup files

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
+import os
 from setuptools import setup, find_packages
 
+# Read README.md for long description
+def read_file(filename):
+    """Read file contents"""
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+    if os.path.exists(filepath):
+        with open(filepath, 'r', encoding='utf-8') as f:
+            return f.read()
+    return ''
+
 setup(
-    name='igs_app_base',
+    name='igs-app-base',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', '*.tests', '*.tests.*']),
+    include_package_data=True,
     author='RRamirez / IMAGILEX',
     author_email='rramirez@rramirez.com',
-    description='Base package for django apps',
-    long_description=open('README.md').read(),
+    description='Base Django app providing core functionality for iGrowSoft applications',
+    long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
     url='https://github.com/imagilex/igs_app_base',
+    project_urls={
+        'Bug Tracker': 'https://github.com/imagilex/igs_app_base/issues',
+        'Documentation': 'https://github.com/imagilex/igs_app_base',
+        'Source Code': 'https://github.com/imagilex/igs_app_base',
+    },
     license='MIT',
+    python_requires='>=3.8',
     install_requires=[
         'Django>=5.1.2',
         'crispy-bootstrap5>=2024.10',
@@ -22,9 +39,31 @@ setup(
         'uritools>=5.0.0',
         'urllib3>=2.4.0',
     ],
+    extras_require={
+        'dev': [
+            'pytest>=7.0',
+            'pytest-django>=4.5',
+            'pytest-cov>=4.0',
+        ],
+    },
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    keywords='django app igrowsoft base',
+    zip_safe=False,
 )
